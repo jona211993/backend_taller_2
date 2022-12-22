25d0ae1faa6b8e34b17ecc1cf645bb7d8f5aac42
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-12-21 19:17:09 -0500

    fix: RoleRepository import RoleEntity

--- src/Repository/roles.repository.ts
+++ src/Repository/roles.repository.ts
@@ -2,42 +2,42 @@ import {NotFound} from "http-errors";
 import database, { } from "../config/database";
 
 import { DatabaseRepository, Id, Query } from "../declarations";
-import { Rol } from "../Entity/rol.entity";
+import { RoleEntity } from "../Entity/rol.entity";
 
-export class RolesRepository implements DatabaseRepository<Rol>{
+export class RolesRepository implements DatabaseRepository<RoleEntity>{
 
-    async create(data: Partial<Rol>, query?: Query | undefined): Promise<Rol> {
-        const repository = database.getRepository(Rol);
+    async create(data: Partial<RoleEntity>, query?: Query | undefined): Promise<RoleEntity> {
+        const repository = database.getRepository(RoleEntity);
         const rol = repository.create(data);
         await repository.save(rol);
         return rol;
     }
 
 
-    async list(query?: Query | undefined): Promise<Rol[]> {
-        const repository = database.getRepository(Rol);
+    async list(query?: Query | undefined): Promise<RoleEntity[]> {
+        const repository = database.getRepository(RoleEntity);
         return repository.find();
     }
 
 
-   async get(id: Id, query?: Query): Promise<Rol> {
-        const repository = database.getRepository(Rol);
+   async get(id: Id, query?: Query): Promise<RoleEntity> {
+        const repository = database.getRepository(RoleEntity);
         const rol = await repository.findOneBy({id_rol: id as any});           
         if (!rol){
-            throw new NotFound ("Rol no existente");}
+            throw new NotFound ("RoleEntity no existente");}
          return rol;
     }
 
 
-    async update(id: Id, data: Rol, query?: Query | undefined): Promise<Rol> {
-        const repository = database.getRepository(Rol);
+    async update(id: Id, data: RoleEntity, query?: Query | undefined): Promise<RoleEntity> {
+        const repository = database.getRepository(RoleEntity);
         await repository.update(id,data);
         return this.get(id,query);
     }
 
 
-    async remove(id: Id, query?: Query | undefined): Promise<Rol> {
-        const repository = database.getRepository(Rol);
+    async remove(id: Id, query?: Query | undefined): Promise<RoleEntity> {
+        const repository = database.getRepository(RoleEntity);
         const rol_eliminado = await this.get(id,query);
         await repository.delete(id);
         return rol_eliminado;

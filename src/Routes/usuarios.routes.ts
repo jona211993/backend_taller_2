127238e4a01676ac74e4859c04281ed479c896ae
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-12-21 19:18:35 -0500

    feat: UserRoute added GetActiveUsers method implemented

--- src/Routes/usuarios.routes.ts
+++ src/Routes/usuarios.routes.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { UsuariosController } from "../Controllers/usuarios.controller";
 
-import {UsuariosRepository} from "../Repository/usuarios.repository"
-const router= Router();
+import { UsuariosRepository } from "../Repository/usuarios.repository";
+const router = Router();
 
-const controller = new UsuariosController(
-    new UsuariosRepository()
-);
+const controller = new UsuariosController(new UsuariosRepository());
 
-router.post("/usuarios", controller.create.bind(controller));
-router.get("/usuarios", controller.list.bind(controller));
-router.get("/usuarios/:id_usuario", controller.get.bind(controller));
-router.put("/usuarios/:id_usuario", controller.update.bind(controller));
-router.delete("/usuarios/:id_usuario",controller.remove.bind(controller))
+const endpoint = "/usuarios";
+
+router.post(`${endpoint}`, controller.create.bind(controller));
+router.get(`${endpoint}`, controller.list.bind(controller));
+router.get(`${endpoint}/active`, controller.getActiveUsers.bind(controller));
+router.get(`${endpoint}/:id_usuario`, controller.get.bind(controller));
+router.put(`${endpoint}/:id_usuario`, controller.update.bind(controller));
+router.delete(`${endpoint}/:id_usuario`, controller.remove.bind(controller));
 // POST
 // GET
 //PUT

1ced7aad7fb7308b33f246fd5717d006225d99ab
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-12-21 19:17:30 -0500

    feat: index.ts try catch added

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
-import "reflect-metadata"
+import "reflect-metadata";
 import express from "express";
-const morgan = require('morgan')
+const morgan = require("morgan");
 
 import database from "./config/database";
 import RolesRoutes from "./Routes/roles.routes";
 import UsuarioRoutes from "./Routes/usuarios.routes";
 
+const main = async () => {
+  try {
+    const app = express();
+    app.use(morgan("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    try {
+      await database.initialize();
+    console.log("Se conectó correctamente la base de datos");
 
-const app=express();
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-database.initialize()
-.then(()=> console.log("Database conectados"))
-.catch(console.error)
-app.use('/api', RolesRoutes)
-app.use('/api', UsuarioRoutes)
+    } catch (e) {
+      console.log("Hubo un error al conectar la base de datos");
+    }
+    app.use("/api", RolesRoutes);
+    app.use("/api", UsuarioRoutes);
 
-app.listen( 3030, () => {
-    console.log("App execute in port 3030")
-});
+    app.listen(3030, () => {
+      console.log("App execute in port 3030");
+    });
+  } catch (error) {
+    console.log("Hubo un error al levantar la base de datos");
+  }
+};
 
-
+main();
